8a531c837a23cb42429dfc384a0bca985aa5957c
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-11-17 18:41:42 -0800

    travis

--- api/tests.py
+++ api/tests.py
@@ -12,7 +12,7 @@ class PostTests(APITestCase):
         self.user = User.objects.create_user(username='bob', email='bob@bob.com', password='password1')
 
     def test_post_list(self):
-        post = Post.objects.create(title='Title', description='Description', user=self.user, location=City.objects.get(pk=1))
+        post = Post.objects.create(title='Title', description='Description', user=self.user, location='las vegas')
         url = reverse('api_post_list_create')
         response = self.client.get(url, {}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -21,7 +21,7 @@ class PostTests(APITestCase):
         self.assertEqual(response_post['title'], post.title)
 
     def test_post_list_request(self):
-        post = Post.objects.create(title='Title', description='Description', user=self.user, location=City.objects.get(pk=1))
+        post = Post.objects.create(title='Title', description='Description', user=self.user, location='las vegas')
         factory = APIRequestFactory()
         view = ListCreatePost.as_view()
         url = reverse('api_post_list_create')
@@ -43,9 +43,9 @@ class PostTests(APITestCase):
         self.assertEqual(self.user.id, response.data['user'])
 
     def test_list_post_username_filter(self):
-        post = Post.objects.create(title='Title', description='Description', user=self.user, location=City.objects.get(pk=1))
+        post = Post.objects.create(title='Title', description='Description', user=self.user, location='las vegas')
         user2 = User.objects.create_user(username='billy', email="billy@billy.com", password='password2')
-        post2 = Post.objects.create(title='Title2', description='Description2', user=user2, location=City.objects.get(pk=1))
+        post2 = Post.objects.create(title='Title2', description='Description2', user=user2, location='las vegas')
         url = reverse('api_post_list_create')
         response = self.client.get(url, {'username': user2.username}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -54,10 +54,10 @@ class PostTests(APITestCase):
         self.assertEqual(post_response['user'], user2.id)
 
     def test_list_top50(self):
-        post = Post.objects.create(title='Title', description='Description', user=self.user, location=City.objects.get(pk=1))
+        post = Post.objects.create(title='Title', description='Description', user=self.user, location='las vegas')
         user2 = User.objects.create_user(username='billy', email="billy@billy.com", password='password2')
-        post2 = Post.objects.create(title='Title2', description='Description2', user=user2, location=City.objects.get(pk=1))
-        post3 = Post.objects.create(title='Title3', description='Description3', user=user2, location=City.objects.get(pk=1))
+        post2 = Post.objects.create(title='Title2', description='Description2', user=user2, location='las vegas')
+        post3 = Post.objects.create(title='Title3', description='Description3', user=user2, location='las vegas')
         fav = Favorite.objects.create(post=post, user=self.user)
         fav2 = Favorite.objects.create(post=post, user=self.user)
         fav3 = Favorite.objects.create(post=post, user=self.user)
