0d292583532829463c15e15497c3481008bf636c
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-11-30 15:29:22 -0800

    static

--- fredslist/settings.py
+++ fredslist/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
